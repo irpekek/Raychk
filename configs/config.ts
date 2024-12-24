4c0a69c9d5d224c7857e2152c83f3ce1244a88fc
irpekek <sandylaksanap@gmail.com>
2024-12-25 01:18:41 +0700

    feat(config): add XrayConfig interface and validation function

---
+++ configs/config.ts
@@ -0,0 +1,21 @@
+import { XrayOutbound } from "./outbound.ts";
+
+export interface XrayConfig {
+  log: object;
+  api?: object;
+  dns?: object;
+  routing: object;
+  policy?: object;
+  inbounds: [];
+  outbounds: XrayOutbound[];
+  stats?: object;
+  reverse?: object;
+  fakedns?: object;
+  metrics?: object;
+  observatory?: object;
+  burstObservatory?: object;
+}
+
+export const isXrayConfig = (obj: unknown): obj is XrayConfig => {
+  return obj !== null && typeof obj === 'object' && 'outbounds' in obj;
+}
