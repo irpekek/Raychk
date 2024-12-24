28ff5be407ab9097d5efc0fad0d8140dc4f2d410
irpekek <sandylaksanap@gmail.com>
2024-12-25 01:18:16 +0700

    feat(types): enhance proxy interfaces and add type guards for protocol validation

--- types.ts
+++ types.ts
@@ -4,33 +4,77 @@ interface Proxy {
   port: number;
   type: string;
   network: string;
-  'skip-cert-verify': boolean;
-  udp: boolean;
+  'skip-cert-verify'?: boolean;
+  udp?: boolean;
   'ws-opts'?: WebsocketOpts;
+  'grpc-opts'?: GrpcOpts;
 }
 interface WebsocketOpts {
   path: string;
   headers: WebsocketHeaders;
-  host?: string
+  host?: string;
 }
 interface WebsocketHeaders {
   host: string;
 }
-interface Trojan extends Proxy {
+interface GrpcOpts {
+  'grpc-service-name': string;
+}
+export interface Trojan extends Proxy {
   sni: string;
   password: string;
 }
-interface Vmess extends Proxy {
+
+export interface Vmess extends Proxy {
   tls: boolean;
   uuid: string;
   alterid: number;
-  cipher: string;
-  servername: string;
+  cipher?: string;
+  servername?: string;
 }
-interface Vless extends Proxy {
+
+export interface Vless extends Proxy {
   tls: boolean;
   uuid: string;
   alterid: number;
-  cipher: string;
-  servername: string;
+  cipher?: string;
+  servername?: string;
 }
+
+
+export const isProxy = (obj: unknown): obj is Proxy => {
+  return obj !== null && typeof obj === 'object' && 'type' in obj;
+};
+export const isVless = (obj: unknown): obj is Vless => {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    'type' in obj &&
+    obj.type === 'vless'
+  );
+};
+export const isVmess = (obj: unknown): obj is Vmess => {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    'type' in obj &&
+    obj.type === 'vmess'
+  );
+};
+export const isTrojan = (obj: unknown): obj is Trojan => {
+  return (
+    obj !== null &&
+    typeof obj === 'object' &&
+    'type' in obj &&
+    obj.type === 'trojan'
+  );
+};
+export const isWebsocket = (config: Vless | Vmess | Trojan): boolean => {
+  return config.network === 'ws' ? true : false;
+};
+export const isGRPC = (config: Vless | Vmess | Trojan): boolean => {
+  return config.network === 'grpc' ? true : false;
+};
+export const isTLS = (config: Vless | Vmess): boolean => {
+  return 'tls' in config && config.tls === true ? true : false;
+};
