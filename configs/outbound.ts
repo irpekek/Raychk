dc8d5b4946428fc87ceeec1b9fa85be0386207f9
irpekek <sandylaksanap@gmail.com>
2024-12-24 17:21:20 +0700

    feat: implement Vless and Trojan protocols within outbound context

--- configs/outbound.ts
+++ configs/outbound.ts
@@ -49,7 +49,7 @@ interface TrojanSetting {
 interface TrojanServer extends BaseServer, TrojanUser {}
 interface TrojanUser {
   password: string;
-  email: string;
+  email?: string;
   level: number;
 }
 interface StreamSetting {
@@ -171,6 +171,137 @@ class Outbound {
       this.save(outbound);
     }
   }
+
+  public vless(
+    name: string,
+    id: string,
+    server: string,
+    port: number,
+    tls = false
+  ) {
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const vless = new Vless(id, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    if (tls) vless.raw(tlsSettings);
+    else vless.raw();
+
+    if (vless.streamSetting && vless.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'vless',
+        settings: vless.setting,
+        streamSettings: vless.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
+
+  public vlessWebsocket(
+    name: string,
+    id: string,
+    server: string,
+    port: number,
+    path: string,
+    host: string,
+    tls = false
+  ) {
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const vless = new Vless(id, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    if (tls) vless.websocket(path, host, tlsSettings);
+    else vless.websocket(path, host);
+
+    if (vless.streamSetting && vless.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'vless',
+        settings: vless.setting,
+        streamSettings: vless.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
+
+  public vlessGRPC(
+    name: string,
+    id: string,
+    server: string,
+    port: number,
+    serviceName: string
+  ) {
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const vless = new Vless(id, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    vless.grpc(serviceName, tlsSettings);
+
+    if (vless.streamSetting && vless.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'vless',
+        settings: vless.setting,
+        streamSettings: vless.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
+
+  public trojan(name: string, password: string, server: string, port: number) {
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const trojan = new Trojan(password, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    trojan.raw(tlsSettings);
+
+    if (trojan.streamSetting && trojan.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'trojan',
+        settings: trojan.setting,
+        streamSettings: trojan.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
+
+  public trojanWebsocket(
+    name: string,
+    password: string,
+    server: string,
+    port: number,
+    path: string,
+    host: string
+  ) {
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const trojan = new Trojan(password, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    trojan.websocket(path, host, tlsSettings);
+
+    if (trojan.streamSetting && trojan.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'trojan',
+        settings: trojan.setting,
+        streamSettings: trojan.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
+
+  public trojanGRPC(name: string, password:string, server: string, port: number, serviceName: string){
+    if (this.hasTag(name)) return console.error(`${name} tag is already taken`);
+    const trojan = new Trojan(password, server, port);
+    const tlsSettings: TLSSetting = generateTLS(server);
+    trojan.grpc(serviceName, tlsSettings);
+
+    if (trojan.streamSetting && trojan.setting) {
+      const outbound: XrayOutbound = {
+        tag: name,
+        protocol: 'trojan',
+        settings: trojan.setting,
+        streamSettings: trojan.streamSetting,
+      };
+      this.save(outbound);
+    }
+  }
 }
 
 class Vmess {
@@ -272,6 +403,202 @@ class Vmess {
   }
 }
 
+class Vless {
+  protected _user: VlessUser | undefined;
+  protected _server: VlessServer | undefined;
+  protected _setting: VlessSetting | undefined;
+  protected _streamSetting: StreamSetting | undefined;
+
+  constructor(
+    private id: string,
+    private address: string,
+    private port: number
+  ) {
+    this.user = { id, encryption: 'none', level: 0, flow: '' };
+    this.server = { address, port, users: [this.user] };
+    this.setting = { vnext: [this.server] };
+  }
+
+  public get streamSetting(): StreamSetting | undefined {
+    return this._streamSetting;
+  }
+
+  private set streamSetting(streamSetting: StreamSetting) {
+    this._streamSetting = streamSetting;
+  }
+
+  public get setting(): VlessSetting | undefined {
+    return this._setting;
+  }
+
+  private set setting(vlessSetting: VlessSetting) {
+    this._setting = vlessSetting;
+  }
+
+  private get server(): VlessServer | undefined {
+    return this._server;
+  }
+
+  private set server(vlessServer: VlessServer) {
+    this._server = vlessServer;
+  }
+
+  private get user(): VlessUser | undefined {
+    return this._user;
+  }
+
+  private set user(vlessUser: VlessUser) {
+    this._user = vlessUser;
+  }
+
+  public raw(tls?: TLSSetting) {
+    let streamSetting: StreamSetting = {
+      network: 'raw',
+      rawSettings: { header: 'none' },
+      security: 'none',
+    };
+    if (tls) streamSetting = this.tls(streamSetting, tls);
+    this.streamSetting = streamSetting;
+  }
+
+  public websocket(path: string, host: string, tls?: TLSSetting) {
+    let streamSetting: StreamSetting = {
+      network: 'ws',
+      wsSettings: { path, host },
+      security: 'none',
+    };
+    if (tls) streamSetting = this.tls(streamSetting, tls);
+    this.streamSetting = streamSetting;
+  }
+
+  public grpc(serviceName: string, tls: TLSSetting) {
+    const streamSetting: StreamSetting = {
+      network: 'grpc',
+      security: 'none',
+      grpcSettings: { serviceName },
+    };
+    this.streamSetting = this.tls(streamSetting, tls);
+  }
+
+  private tls(streamSettings: StreamSetting, tls: TLSSetting): StreamSetting {
+    return {
+      ...streamSettings,
+      network: streamSettings.network,
+      security: 'tls',
+      tlsSettings: tls,
+    };
+  }
+
+  private sockOpts(streamSetting: StreamSetting): StreamSetting {
+    return {
+      ...streamSetting,
+      network: streamSetting.network,
+      sockopt: {
+        mark: 0,
+        tcpFastOpen: true,
+        tproxy: 'tproxy',
+      },
+    };
+  }
+}
+
+class Trojan {
+  protected _user: TrojanUser | undefined;
+  protected _server: TrojanServer | undefined;
+  protected _setting: TrojanSetting | undefined;
+  protected _streamSetting: StreamSetting | undefined;
+
+  constructor(
+    private password: string,
+    private address: string,
+    private port: number
+  ) {
+    this.user = { password, level: 0 };
+    this.server = { address, port, ...this.user };
+    this.setting = { servers: [this.server] };
+  }
+
+  public get streamSetting(): StreamSetting | undefined {
+    return this._streamSetting;
+  }
+
+  private set streamSetting(streamSetting: StreamSetting) {
+    this._streamSetting = streamSetting;
+  }
+
+  public get setting(): TrojanSetting | undefined {
+    return this._setting;
+  }
+
+  private set setting(trojanSetting: TrojanSetting) {
+    this._setting = trojanSetting;
+  }
+
+  private get server(): TrojanServer | undefined {
+    return this._server;
+  }
+
+  private set server(trojanServer: TrojanServer) {
+    this._server = trojanServer;
+  }
+
+  private get user(): TrojanUser | undefined {
+    return this._user;
+  }
+
+  private set user(trojanUser: TrojanUser) {
+    this._user = trojanUser;
+  }
+
+  public raw(tls: TLSSetting) {
+    const streamSetting: StreamSetting = {
+      network: 'raw',
+      rawSettings: { header: 'none' },
+      security: 'none',
+    };
+    this.streamSetting = this.tls(streamSetting, tls);
+  }
+
+  public websocket(path: string, host: string, tls: TLSSetting) {
+    const streamSetting: StreamSetting = {
+      network: 'ws',
+      wsSettings: { path, host },
+      security: 'none',
+    };
+    this.streamSetting = this.tls(streamSetting, tls);
+  }
+
+  public grpc(serviceName: string, tls: TLSSetting) {
+    const streamSetting: StreamSetting = {
+      network: 'grpc',
+      security: 'none',
+      grpcSettings: { serviceName },
+    };
+    this.streamSetting = this.tls(streamSetting, tls);
+  }
+
+  private tls(streamSettings: StreamSetting, tls: TLSSetting): StreamSetting {
+    return {
+      ...streamSettings,
+      network: streamSettings.network,
+      security: 'tls',
+      tlsSettings: tls,
+    };
+  }
+
+  private sockOpts(streamSetting: StreamSetting): StreamSetting {
+    return {
+      ...streamSetting,
+      network: streamSetting.network,
+      sockopt: {
+        mark: 0,
+        tcpFastOpen: true,
+        tproxy: 'tproxy',
+      },
+    };
+  }
+}
+
 function generateTLS(server: string): TLSSetting {
   return {
     allowInsecure: true,
@@ -280,3 +607,4 @@ function generateTLS(server: string): TLSSetting {
     fingerprint: 'chrome',
   };
 }
+
