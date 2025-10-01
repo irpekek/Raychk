ede2c33b093152ab93cfd40213bdf638cdeb5ccb
irpekek <sandylaksanap@gmail.com>
2025-10-01 22:38:50 +0700

    refactor: just styling change from formatter

--- utils/ipinfo.ts
+++ utils/ipinfo.ts
@@ -14,15 +14,18 @@ const isIPInfo = (obj: unknown): obj is IPInfo => {
 
 const URI = 'http://ipinfo.io/json';
 
-export const infoIP = async (host: string,  port: number): Promise<IPInfo | null> => {
+export const infoIP = async (
+  host: string,
+  port: number,
+): Promise<IPInfo | null> => {
   try {
     const client = Deno.createHttpClient({
       proxy: {
         url: `http://${host}:${port}`,
-        transport: "http"
-      }
-    })
-    const response = await fetch(URI, {client});
+        transport: 'http',
+      },
+    });
+    const response = await fetch(URI, { client });
     if (!response.ok) return null;
     const data = await response.json();
     if (!isIPInfo(data)) return null;
